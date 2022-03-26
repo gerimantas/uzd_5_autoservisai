10019664f58d5eab658cf2fad232b3a1f15be51c
Gerimantas <gerimantas.v@gmail.com>
2022-03-26 10:44:20 +0200

    Sukurtas projektas

---
+++ src/test/java/lt/autoservis/u5/Praktika5ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.autoservis.u5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktika5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
